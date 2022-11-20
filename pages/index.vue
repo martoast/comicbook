7cc0320e5b57db3e16262b2f85252be2d76116c4
martoast <alexmartos96@gmail.com>
2022-11-20 07:34:45 -0800

    comicbook

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,58 @@
 <template>
   <div style="background-color: #000 !important; height: 100vh">
-    <client-only>
-      <Flipbook
-        class="flipbook"
-        :pages="pages"
-        v-slot="flipbook"
-        ref="flipbook"
-      >
-      </Flipbook>
-    </client-only>
-    <b-modal
-      id="wallet-modal"
-      centered
-      hide-header
-      hide-header-close
-      hide-footer
-      no-close-on-backdrop
-      no-close-on-esc
-      size="lg"
-    >
-      <b-container>
-        <div class="text-center">
-          <h2 class="py-3" style="font-family: 'Market'; font-size: 42px">
-            Comic book Vol.1
-          </h2>
-
-          <p style="font-family: 'Champange'; font-size: 22px" class="pb-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id
-            interdum urna, ut egestas ex. Curabitur est ex, vehicula a dolor
-            vitae, varius mattis turpis.
-          </p>
-
-          <b-btn
-            v-if="!initialState.account"
-            @click="connectWallet"
-            style="font-family: 'Market'; background-color: black"
-            >Connect wallet</b-btn
-          >
-
-          <b-btn
-            v-else-if="initialState.amount_holding > 0"
-            @click="onEnterComic"
-            style="font-family: 'Market'; background-color: black"
-          >
-            Enter
-          </b-btn>
-
-          <h2
-            v-else-if="initialState.amount_holding == 0"
-            style="font-family: 'Market'"
-          >
-            Must be a holder to enter.
-          </h2>
-
-          <div v-else>
-            <b-spinner label="Spinning"></b-spinner>
-          </div>
-        </div>
-      </b-container>
-    </b-modal>
+     <client-only>
+        <Flipbook
+           class="flipbook"
+           :pages="pages"
+           ref="flipbook"
+           >
+        </Flipbook>
+     </client-only>
+     <b-modal
+        id="wallet-modal"
+        centered
+        hide-header
+        hide-header-close
+        hide-footer
+        no-close-on-backdrop
+        no-close-on-esc
+        size="lg"
+        >
+        <b-container>
+           <div class="text-center">
+              <h2 class="py-3" style="font-family: 'wordofgod'; font-size: 42px">
+                 Comic book Vol.1
+              </h2>
+              <p style="font-family: 'wordofgod'; font-size: 22px" class="pb-3">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id
+                 interdum urna, ut egestas ex. Curabitur est ex, vehicula a dolor
+                 vitae, varius mattis turpis.
+              </p>
+              <b-btn
+                 v-if="!initialState.account"
+                 @click="connectWallet"
+                 style="font-family: 'wordofgod'; background-color: black; font-size:26;"
+                 >Connect wallet</b-btn
+                 >
+              <b-btn
+                 v-else-if="initialState.amount_holding > 0"
+                 @click="onEnterComic"
+                 style="font-family: 'wordofgod'; background-color: black; font-size:26;"
+                 >
+                 Enter
+              </b-btn>
+              <h2
+                 v-else-if="initialState.amount_holding == 0"
+                 style="font-family: 'Market'"
+                 >
+                 Must be a holder to enter.
+              </h2>
+              <div v-else>
+                 <b-spinner label="Spinning"></b-spinner>
+              </div>
+           </div>
+        </b-container>
+     </b-modal>
   </div>
 </template>
 
@@ -78,6 +72,34 @@ export default {
         "/images/2.png",
         "/images/3.png",
         "/images/4.png",
+        "/images/5.png",
+        "/images/6.png",
+        "/images/7.png",
+        "/images/8.png",
+        "/images/9.png",
+        "/images/10.png",
+        "/images/11.png",
+        "/images/12.png",
+        "/images/13.png",
+        "/images/14.png",
+        "/images/15.png",
+        "/images/16.png",
+        "/images/17.png",
+        "/images/18.png",
+        "/images/19.png",
+        "/images/20.png",
+        "/images/21.png",
+        "/images/22.png",
+        "/images/23.png",
+        "/images/24.png",
+        "/images/25.png",
+        "/images/26.png",
+        "/images/27.png",
+        "/images/28.png",
+        "/images/29.png",
+        "/images/30.png",
+        "/images/31.png",
+        null
       ],
       ethereum: null,
       metamaskIsInstalled: false,
@@ -173,6 +195,15 @@ export default {
   font-display: swap;
   src: url("~assets/fonts/Market.ttf") format("truetype");
 }
+
+@font-face {
+  font-family: "WordOfGod";
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~assets/fonts/WordOfGod.ttf") format("truetype");
+}
+
 
 .flipbook {
   width: 100vw;
